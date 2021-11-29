6d961f3b02f4ea8d422824c42663fa94eda5c285
Mihai Rosian <mihai.rosian@cpp.canon>
2021-11-29 13:14:33 +0200

    Removed web request and made read from file

--- script.ps1
+++ script.ps1
@@ -1,27 +1,24 @@
-$icon_url = $args[0]
+$icon_path = $args[0]
 $outputPath = $args[1]
 $localPath = (Get-Location).Path
-
-# optional, on MacOS icomoon-cli failed to launch Chrome
-$env:CHROME_PATH = (Get-Process -Name "Google Chrome")[0].Path
 
 # Create temp dir
 New-Item -ItemType Directory -Force -Path .\temp\icons_all
 
-# Download zip
-Invoke-WebRequest -Uri $icon_url -OutFile ".\temp\temp.zip"
+# Make zip from svg files in a folder
+Compress-Archive $icon_path -DestinationPath .\temp\temp.zip
 
 # Extract zip file
 Expand-Archive .\temp\temp.zip -DestinationPath .\temp\temp_icons
 
 # Collect all svg files into 1 folder
-Get-ChildItem -Path ".\temp\temp_icons\icons\*.svg" -Recurse | Move-Item -Destination ".\temp\icons_all" 
+Get-ChildItem -Path ".\temp\temp_icons\*.svg" -Recurse | Move-Item -Destination ".\temp\icons_all" 
 
 # Get a csv of svg files
 $iconList = (Get-ChildItem -Path '.\temp\icons_all' ).FullName -join ","
 
 # Run iconmoon-cli
-icomoon-cli -i $iconList -s $localPath/selection-empty.json -o $outputPath
+icomoon-cli -i $iconList -s $localPath/selection-empty.json -o $outputPath -f 
 
 # cleanup
 Remove-Item .\temp -Recurse -ErrorAction Ignore
